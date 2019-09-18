2521c6d6eb4cdd55d73ac310aec0caa92c0932e6
Junwei Deng <theaperdeng@outlook.com>
2019-09-18 18:38:19 -0400

    quit function and ? check is complete

--- magic_eight.py
+++ magic_eight.py
@@ -2,3 +2,7 @@
 # !/usr/bin/python3
 
 question = input("What is your question? ")
+while(question != "quit"):
+    if (len(question) == 0 or question[-1] != "?"):
+        print("I’m sorry, I can only answer questions.")
+    question = input("What is your question? ")
