2c1c21fd80d092180ab4dfc566d63793961051fc
Junwei Deng <theaperdeng@outlook.com>
2019-09-22 09:54:08 -0400

    add answer function call

--- magic_eight.py
+++ magic_eight.py
@@ -5,4 +5,6 @@ question = input("What is your question? ")
 while(question != "quit"):
     if (len(question) == 0 or question[-1] != "?"):
         print("I’m sorry, I can only answer questions.")
+    else:
+        answer_question()
     question = input("What is your question? ")
